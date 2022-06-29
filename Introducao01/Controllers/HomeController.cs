06b8744e804d8669b0ee8873e8913880d5492492
jmarciosilva <jmarciosilva@gmail.com>
2022-06-29 13:37:29 -0300

    Foi criado o projeto, Criado ControllerHome, criado 3 views index, contatos e teste, também criado links com Helper activeLink

---
+++ Introducao01/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Introducao01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+
+       
+    }
+}
